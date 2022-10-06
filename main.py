92f2565a0c6e02efa2eadd058264e63dcd0d29fb
ianmarquez1129 <ianmarquez1129@gmail.com>
2022-10-06 19:45:31 +0800

    Save as mp4 video

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+fourcc = cv2.VideoWriter_fourcc("m", "p", "4", "v")
+out = cv2.VideoWriter('sample.mp4', fourcc, 20.0, (640, 480))
+
 while True:
 	_, frame = cap.read()
 	cv2.imshow("frame", frame)
+	out.write(frame)
 	if cv2.waitKey(30) == 27:
 		break
 
