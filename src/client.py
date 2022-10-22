09f9b4a55b48c1446ea1938bd5278f5357c0dc30
zEuS0390 <zeusjamesbaltazar@gmail.com>
2022-10-22 12:28:44 +0800

    Set on_message function for mqtt_client

--- src/client.py
+++ src/client.py
@@ -5,15 +5,15 @@ import time, json
 
 class MQTTClient:
 
-    def __init__(self, cfg: ConfigParser, cfg_section: str, on_message=False, port=1883):
+    def __init__(self, cfg: ConfigParser, cfg_section: str, on_message=None, port=1883):
         self.client_id = cfg.get(cfg_section, "client_id_name")
         self.topic = cfg.get(cfg_section, "topic_name")
         self.broker = cfg.get(cfg_section, "broker_ip")
         self.port = port
         self.client = Client(client_id=self.client_id)
         self.client.on_connect = self.on_connect
-        if on_message:
-            self.client.on_message = self.on_message
+        if on_message is not None:
+            self.client.on_message = on_message
         
     def start(self):
         self.client.connect(self.broker, self.port)
