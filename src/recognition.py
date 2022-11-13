61d6d0c6d5e8530f984819241755975fec07272a
Zeus James <39390245+zEuS0390@users.noreply.github.com>
2022-11-14 00:03:55 +0800

    Update recognition.py

--- src/recognition.py
+++ src/recognition.py
@@ -29,3 +29,30 @@ class Recognition:
         # Predict classes and remove classifications that aren't within the threshold
         return [(pred, loc) if rec else (-1, loc) for pred, loc, rec in zip(predictions, X_face_locations, are_matches)]
     
+    def train(train_dir, model_save_path=None, knn_algo='ball_tree', verbose=False):
+        X = []
+        y = []
+        for class_dir in os.listdir(train_dir):
+            if not os.path.isdir(os.path.join(train_dir, class_dir)):
+                continue
+            for img_path in image_files_in_folder(os.path.join(train_dir, class_dir)):
+                image = face_recognition.load_image_file(img_path)
+                face_bounding_boxes = face_recognition.face_locations(image, model="cnn")
+                if len(face_bounding_boxes) != 1:
+                    if verbose:
+                        print("Image {} not suitable for training: {}".format(img_path, "Didn't find a face" if len(face_bounding_boxes) < 1 else "Found more than one face"))
+                else:
+                    X.append(face_recognition.face_encodings(image, known_face_locations=face_bounding_boxes)[0])
+                    y.append(class_dir)
+                    if verbose:
+                      print("Image {}: Found face".format(img_path))
+        n_neighbors = int(round(math.sqrt(len(X))))
+        if verbose:
+            print("Chose n_neighbors automatically:", n_neighbors)
+        knn_clf = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, algorithm=knn_algo, weights='distance')
+        knn_clf.fit(X, y)
+        # Save the trained KNN classifier
+        if model_save_path is not None:
+            with open(model_save_path, 'wb') as f:
+                pickle.dump(knn_clf, f)
+        return knn_clf
