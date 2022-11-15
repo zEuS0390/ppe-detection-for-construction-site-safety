f49886fa5ece21f4dd3256a8b12abe71a8e2c09b
zEuS0390 <zeusjamesbaltazar@gmail.com>
2022-11-15 18:34:09 -0500

    Migrate main function as a module

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-from src.db.database import DatabaseHandler
-from src.db.crud import *
-from src.db.tables import *
-from sqlalchemy import select
-from src.client import MQTTClient
-from src.detection import Detection
-from src.utils import checkLatestWeights
-from src.recognition import Recognition
-from src.camera import Camera
-import configparser, time
+from src.app import Application
 
 """
     TECHNOLOGICAL INSTITUTE OF THE PHILIPPINES - QUEZON CITY
@@ -19,36 +10,5 @@ import configparser, time
         - MARQUEZ, IAN GABRIEL
 """
 
-def main():
-
-    # Check latest weights file
-    checkLatestWeights()
-
-    # Load app configuration file
-    cfg = configparser.ConfigParser()
-    cfg.read("./cfg/app.cfg")
-
-    # Instantiate objects
-    database = DatabaseHandler(cfg=cfg)
-    mqtt_notif = MQTTClient("notif")
-    mqtt_set = MQTTClient("set")
-    recognition = Recognition(cfg)
-    camera = Camera(cfg)
-    insertPersons(database, cfg.get("face_recognition", "persons"))
-    insertPPEClasses(database, cfg.get("yolor","classes"))
-    detection = Detection(cfg, database, camera, recognition, mqtt_notif, mqtt_set)
-
-    # Start threads
-    mqtt_notif.start()
-    mqtt_set.start()
-    camera.start()
-    detection.start()
-
-    try:
-        while True: time.sleep(1)
-    except:
-        detection.isRunning = False
-        camera.isRunning = False
-
 if __name__=="__main__":
-    main()
+    Application.main()
---
+++ src/app.py
@@ -0,0 +1,44 @@
+from src.db.database import DatabaseHandler
+from src.db.crud import *
+from src.db.tables import *
+from sqlalchemy import select
+from src.client import MQTTClient
+from src.detection import Detection
+from src.utils import checkLatestWeights
+from src.recognition import Recognition
+from src.camera import Camera
+import configparser, time
+
+class Application:
+
+    @staticmethod
+    def main():
+        
+        # Check latest weights file
+        checkLatestWeights()
+
+        # Load app configuration file
+        cfg = configparser.ConfigParser()
+        cfg.read("./cfg/app.cfg")
+
+        # Instantiate objects
+        database = DatabaseHandler(cfg=cfg)
+        mqtt_notif = MQTTClient("notif")
+        mqtt_set = MQTTClient("set")
+        recognition = Recognition(cfg)
+        camera = Camera(cfg)
+        insertPersons(database, cfg.get("face_recognition", "persons"))
+        insertPPEClasses(database, cfg.get("yolor","classes"))
+        detection = Detection(cfg, database, camera, recognition, mqtt_notif, mqtt_set)
+
+        # Start threads
+        mqtt_notif.start()
+        mqtt_set.start()
+        camera.start()
+        detection.start()
+
+        try:
+            while True: time.sleep(1)
+        except:
+            detection.isRunning = False
+            camera.isRunning = False
