f9d7e5e1998296a1b2a9f6502491ddc32eb4cec7
zEuS0390 <zeusjamesbaltazar@gmail.com>
2022-12-07 00:18:04 +0800

    Display the values of configurations

--- contrib/configure.py
+++ contrib/configure.py
@@ -1,6 +1,6 @@
 from configparser import ConfigParser
 from getpass import getpass
-import os
+import os, sys
 
 APP_CFG_FILENAME = "cfg/app.cfg"
 DETECTION_CFG_FILENAME = "cfg/detection/config.cfg"
@@ -13,7 +13,10 @@ detection_cfg = ConfigParser()
 detection_cfg.read(DETECTION_CFG_FILENAME)
 
 def clear():
-    os.system("clear")
+    if sys.platform == "linux":
+        os.system("clear")
+    elif sys.platform == "win32":
+        os.system("cls")
 
 def parsePlainConfig(filepath: str):
     if not os.path.exists(filepath):
@@ -53,6 +56,22 @@ def setPlainConfig(filepath, target_key, new_value):
     except Exception as e:
         raise e
 
+def getPlainConfig(filepath, target_key):
+    if not os.path.exists(filepath):
+        raise Exception(f"'{filepath}' does not exist.")
+    try:
+        cfg = {}
+        with open(filepath, "r") as file:
+            lines = file.readlines()
+            for line in lines:
+                separator_index = line.find(":")
+                key = line[:separator_index]
+                value = line[separator_index+1:].strip()
+                cfg[key] = value
+        return cfg
+    except Exception as e:
+        raise e
+
 class Configure:
 
     def __init__(self):
@@ -71,8 +90,8 @@ class Configure:
             return
         options[int(selected)-1][1]()
 
-    def displayOptions(self, title, options):
-        buffer = f"{title} Options:\n"
+    def displayOptions(self,options):
+        buffer = "Options:\n"
         for index, option in enumerate(options):
             buffer += f"{index+1}. {option[0].title()}\n"
         print(buffer, end="")
@@ -93,7 +112,11 @@ class Configure:
             ("name", setName), 
             ("description", setDescription)
         ]
-        self.displayOptions("Camera Details", options)
+        print("Camera Details")
+        print("Current Values:")
+        print("NAME:", app_cfg.get("camera", "name"))
+        print("DESCRIPTION:", app_cfg.get("camera", "description"))
+        self.displayOptions(options)
         self.executeSelectedOption(options)
         self.selectOption()
 
@@ -123,7 +146,13 @@ class Configure:
             ("Boots", lambda: setPPEPreferenceState("boots")),
             ("No Boots", lambda: setPPEPreferenceState("no boots")),
         ]
-        self.displayOptions("PPE Preference", options)
+        print("PPE Preference")
+        print("Current Values:")
+        for index in range(len(options)):
+            ppe_item = options[index][0].lower().replace(" ", "_")
+            status = "on" if detection_cfg.getboolean("ppe_preferences", ppe_item) else "off"
+            print(f"{options[index][0].upper()} = {status}")
+        self.displayOptions(options)
         self.executeSelectedOption(options)
         self.selectOption()
 
@@ -137,7 +166,7 @@ class Configure:
             setPlainConfig(MQTT_CLIENT_NOTIF_FILENAME, "topic_name", client_name+"/notif")
             setPlainConfig(MQTT_CLIENT_SET_FILENAME, "topic_name", client_name+"/set")
         def setHostName():
-            ipaddr = input("Enter hostname: ")
+            ipaddr = input("Enter broker ip: ")
             setPlainConfig(MQTT_CLIENT_NOTIF_FILENAME, "broker_ip", ipaddr)
             setPlainConfig(MQTT_CLIENT_SET_FILENAME, "broker_ip", ipaddr)
         def setUserName():
@@ -149,12 +178,21 @@ class Configure:
             setPlainConfig(MQTT_CLIENT_NOTIF_FILENAME, "password", password)
             setPlainConfig(MQTT_CLIENT_SET_FILENAME, "password", password)
         options = [
-            ("client name", setClientName),
-            ("hostname", setHostName),
+            ("client id name", setClientName),
+            ("broker ip", setHostName),
             ("username", setUserName),
             ("password", setPassword)
         ]
-        self.displayOptions("MQTT Connection", options)
+        print("MQTT Connection")
+        print("Current Values:")
+        current_values = getPlainConfig(MQTT_CLIENT_NOTIF_FILENAME, "client_id_name")
+        client_id_name_last_index = current_values["client_id_name"].find("-notif")
+        current_values["client_id_name"] = current_values["client_id_name"][:client_id_name_last_index] if client_id_name_last_index != -1 else "None"
+        current_values["password"] = len(current_values["password"])*"*"
+        for index in range(len(options)):
+            item = options[index][0].lower().replace(" ", "_")
+            print(f"{options[index][0].upper()} = {current_values[item]}")
+        self.displayOptions(options)
         self.executeSelectedOption(options)
         self.selectOption()
 
@@ -170,7 +208,8 @@ class Configure:
             ("mqtt connection", self.configureMQTTConnection),
             ("sftp connection", self.configureSFTPConnection)
         ]
-        self.displayOptions("Application", configuration_options)
+        print("Application")
+        self.displayOptions(configuration_options)
         self.executeSelectedOption(configuration_options)
 
 if __name__=="__main__":
