2f5617f63a286e9fdb811d955518c7d014b37f7a
zEuS0390 <zeusjamesbaltazar@gmail.com>
2022-12-11 22:53:47 +0800

    Create only one instance on certain classes using singleton

---
+++ src/singleton.py
@@ -0,0 +1,13 @@
+class Singleton(type):
+
+    _instances = {}
+
+    def __call__(_cls, *args, **kwargs):
+        if _cls not in _cls._instances:
+            _cls._instances[_cls] = super(Singleton, _cls).__call__(*args, **kwargs)
+        return _cls._instances[_cls]
+
+    def getInstance(_cls) -> any:
+        if _cls in _cls._instances:
+            return _cls._instances[_cls]
+        raise KeyError(f"Instance of '{_cls.__name__}' does not exist!")
