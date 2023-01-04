6734a64e5e05bdf4b5e747ac8ded176612a29bbb
Zeus James <39390245+zEuS0390@users.noreply.github.com>
2023-01-04 16:20:38 +0800

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -20,18 +20,18 @@ class BGRColor(Enum):
     """
     Color in BGR format
     """
-    RED = (0, 0, 255)                   # 0
-    ORANGE = (0, 165, 255)              # 1
-    YELLOW = (0, 255, 255)              # 2
-    GREEN = (0, 255, 0)                 # 3
-    SPRING_GREEN = (124, 208, 38)       # 4
-    CYAN = (255, 255, 0)                # 5
-    AZURE = (255, 127, 0)               # 6
-    BLUE = (255, 0, 0)                  # 7
-    VIOLET = (128, 0, 128)              # 8
-    MAGENTA = (255, 0, 255)             # 9
-    ROSE = (127, 0, 255)                # 10
-    BISQUE = (242, 210, 189)            # 11
+    RED = (0, 0, 255)
+    ORANGE = (0, 165, 255)
+    YELLOW = (0, 255, 255)
+    GREEN = (0, 255, 0)
+    SPRING_GREEN = (124, 208, 38)
+    CYAN = (255, 255, 0)
+    AZURE = (255, 127, 0)
+    BLUE = (255, 0, 0)
+    VIOLET = (128, 0, 128)
+    MAGENTA = (255, 0, 255)
+    ROSE = (127, 0, 255)
+    BISQUE = (242, 210, 189)
 
 class RGBColor(Enum):
     """
