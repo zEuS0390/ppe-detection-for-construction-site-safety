9778b28bb506e5cc1d5b6c812a70021aa5855fb5
zEuS0390 <zeusjamesbaltazar@gmail.com>
2023-01-20 00:22:04 +0800

    Initial implementation of xlsx file export

--- src/export.py
+++ src/export.py
@@ -1,3 +1,14 @@
 from src.db.crud import DatabaseCRUD
+from datetime import datetime
+from openpyxl import Workbook
+import os
 
-def exportAsXLSX(*args, **kwargs): ...
+def exportAsXLSX(path_to_directory: str = "data/export", 
+                 from_datetime: datetime = datetime.now().strftime("%Y-%m-%d 00:00:00"), 
+                 to_datetime: datetime = datetime.now().strftime("%Y-%m-%d 11:59:59")):
+    db = DatabaseCRUD.getInstance()
+    list_of_violation_details = db.getAllViolationDetails(from_datetime, to_datetime)
+    wb = Workbook()
+    ws = wb.active
+    filename = os.path.join(path_to_directory, from_datetime.replace(" ", "_").replace(":", "-")+"_TO_"+to_datetime.replace(" ", "_").replace(":", "-")+".xlxs")
+    wb.save(filename)
