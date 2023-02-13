6179c777c9977418a3197eed1c07ab210670cd51
Zeus James <39390245+zEuS0390@users.noreply.github.com>
2023-02-13 23:57:32 +0800

    Update box.py

--- src/box.py
+++ src/box.py
@@ -2,12 +2,18 @@ from dataclasses import dataclass
 
 @dataclass
 class Box:
+    """
+    A class that contains coordinates of the bounding box
+    """
     top: int
     right: int
     bottom: int
     left: int
 
 def isColliding(box_1: Box, box_2: Box):
+    """
+    Check collision between the bounding box edges by using AABB (Axis-Aligned Bounding Box) algorithm
+    """
     if box_1.left < box_2.right and box_1.right > box_2.left and box_1.bottom > box_2.top and box_1.top < box_2.bottom:
         return True
     return False
