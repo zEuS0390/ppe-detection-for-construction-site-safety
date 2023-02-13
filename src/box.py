7def65ed4df71f38bff1bdf17bbceb746b7c243f
Zeus James <39390245+zEuS0390@users.noreply.github.com>
2023-02-13 23:58:27 +0800

    Update box.py

--- src/box.py
+++ src/box.py
@@ -12,7 +12,7 @@ class Box:
 
 def isColliding(box_1: Box, box_2: Box):
     """
-    Check collision between the bounding box edges by using AABB (Axis-Aligned Bounding Box) algorithm
+    Check collision between the bounding box's edges by using AABB (Axis-Aligned Bounding Box) algorithm
     """
     if box_1.left < box_2.right and box_1.right > box_2.left and box_1.bottom > box_2.top and box_1.top < box_2.bottom:
         return True
