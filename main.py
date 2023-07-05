3ad8717ee19b302736fd7ae8db4abbea61bb159e
zEuS0390 <zeusjamesbaltazar@gmail.com>
2023-07-06 00:05:10 +0800

    feat: separate cloud and local options

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from src.app import Application
+from argparse import ArgumentParser
+import sys
 
 """
     TECHNOLOGICAL INSTITUTE OF THE PHILIPPINES - QUEZON CITY
@@ -14,4 +15,20 @@ from src.app import Application
 # It calls the static method of the Application
 # module to run the main process.
 if __name__=="__main__":
-    Application.main()
+    parser = ArgumentParser()
+    parser.add_argument(
+            "-l", 
+            "--local", 
+            action="store_true", 
+            help="Change the mode of the application is local or cloud based"
+    )
+    args = parser.parse_args()
+    if args.local:
+        from src import localapp
+        print("Initializing local application")
+        localapp.Application.main()
+    else:
+        from src import cloudapp
+        print("Initializing cloud application")
+        cloudapp.Application.main()
+
---
+++ src/cloudapp.py
@@ -0,0 +1,5 @@
+class Application:
+
+    @staticmethod
+    def main():
+        print("I'm in using cloud to utilize this application")
